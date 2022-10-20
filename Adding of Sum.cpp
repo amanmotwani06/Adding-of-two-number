e8bafdd0ee34768267ab3ec9104974225b8b4d54
amanmotwani06 <115036142+amanmotwani06@users.noreply.github.com>
2022-10-20 20:45:46 +0530

    Add files via upload

---
+++ Adding of Sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1, num2;
+	cout<<"Enter Value of num1 : \n"; /* '<<' this is insertion value*/
+	cin>>num1; /* '>>' this is extraction value*/
+	cout<<"Enter Value of num2 : \n"; /* '<<' this is insertion value*/
+	cin>>num2; /* '>>' this is extraction value*/
+	cout<<"the sum is " <<num1+num2;
+	}
